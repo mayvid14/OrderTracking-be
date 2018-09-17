34ef98397af3275be17157cc0689eddcc4f395c1
Divyam Nandanwar <divyam.nandanwar@infogain.com>
2018-09-17 16:30:34 +0530

    first commit

---
+++ src/main/java/com/ot/OrderTrackingBeApplication.java
@@ -0,0 +1,12 @@
+package com.ot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderTrackingBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderTrackingBeApplication.class, args);
+	}
+}
